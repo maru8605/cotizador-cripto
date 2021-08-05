6460e2a375cdd4d7aac973eb349f8646147a1eaf
maru8605 <maru8605@hotmail.com>
2021-08-05 16:28:35 -0300

    estilos del C hook

--- src/hooks/useMoney.js
+++ src/hooks/useMoney.js
@@ -1,4 +1,25 @@
 import { useState } from "react"
+import styled from '@emotion/styled'
+
+const Label = styled.label`
+    font-family: 'Bebas Neue', cursive;
+    color: #fff;
+    text-transform: uppercase;
+    font-weight:bold;
+    font-size: 2.4rem;
+    margin-top: 2rem;
+    display: block;
+`;
+
+const ISelect = styled.select`
+    width: 100%;
+    display: block;
+    padding: 1rem;
+    font-size: 1.2rem;
+    border-radius:10px;
+    border: none;
+    --webkit-appearance: none;
+`
 
 const useMoney = (label, stateInicial, opciones) => {
 // State del custom hook
@@ -6,8 +27,8 @@ const useMoney = (label, stateInicial, opciones) => {
 
     const Select = () => (
         <>
-            <label>{label}</label>
-            <select 
+            <Label>{label}</Label>
+            <ISelect 
              onChange={ e => setState(e.target.value)}
              value={state}
             >
@@ -15,7 +36,7 @@ const useMoney = (label, stateInicial, opciones) => {
                 {opciones.map(opcion => (
                     <option key={opcion.codigo} value={opcion.codigo}>-{opcion.nombre}</option>
                 ))}
-            </select>
+            </ISelect>
         </>
     )
 
